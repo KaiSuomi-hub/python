d97e7941ea11bddb18ae784cb89537df3b7bc797
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2020-11-27 17:47:23 +0200

    toistoa toistoa

---
+++ spiral.py
@@ -0,0 +1,29 @@
+# spcoord.py
+from sense_hat import SenseHat
+import time
+import random
+sense = SenseHat()
+
+
+def spiral(X, Y):
+    x = y = 0
+    dx = 0
+    dy = -1
+    c = [random.randrange(0, 255), random.randrange(
+        0, 255), random.randrange(0, 255)]
+
+    for i in range(max(X, Y)**2):
+        if (-X/2 < x <= X/2) and (-Y/2 < y <= Y/2):
+            sense.set_pixel(abs(x-4), abs(y-4), c)
+            time.sleep(0.1)
+        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
+
+
+try:
+    spiral(8, 8)
+    sense.clear()
+
+except KeyboardInterrupt:
+    sense.clear()
